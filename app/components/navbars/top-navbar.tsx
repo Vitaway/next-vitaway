c42a24c9be8d30f0e0cc24f4e1b92d8389903076
patiencemanzen <hseal419@gmail.com>
2025-04-23 16:22:14 +0200

    feat: Update bottom navbar layout and links, add appointment page and form
    - Refactored bottom navbar for improved layout and responsiveness.
    - Updated navigation links and added a shopping cart icon.
    - Changed contact email in top navbar to new address.
    - Modified pricing JSON to update call-to-action button text and link.
    - Created a new appointment page with metadata and layout.
    - Implemented appointment form with validation and submission handling.

--- app/components/navbars/top-navbar.tsx
+++ app/components/navbars/top-navbar.tsx
@@ -17,9 +17,9 @@ function TopNavbar() {
 
                     <span className="text-body top-header-about-links ml-2">|</span>
 
-                    <a className="text-decoration-none text-body px-3  font-bold flex items-center text-sm" rel="noopener" target="__blank" href="mailto:info@vitaway.org">
+                    <a className="text-decoration-none text-body px-3  font-bold flex items-center text-sm" rel="noopener" target="__blank" href="mailto:vitawayeclinic@gmail.com">
                         <div className='border border-gray-200 rounded-xl p-[5px]'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"><path d="M17 20.5H7c-3 0-5-1.5-5-5v-7c0-3.5 2-5 5-5h10c3 0 5 1.5 5 5v7c0 3.5-2 5-5 5Z" stroke="#697689" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path><path opacity=".4" d="m17 9-3.13 2.5c-1.03.82-2.72.82-3.75 0L7 9" stroke="#697689" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path></svg></div>
-                        <span className='ml-2 hidden lg:block'>info@vitaway.org</span>
+                        <span className='ml-2 hidden lg:block'>vitawayeclinic@gmail.com</span>
                     </a>
 
                     <span className="text-body top-header-about-links">|</span>
