36703304435444632cd34567e2ca6285326d5df1
patiencemanzen <hseal419@gmail.com>
2025-05-08 14:37:46 +0200

    feat: Update services section with enhanced offerings and improved layout

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,82 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "cdn.rareblocks.xyz",
-      "res.cloudinary.com",
-      "cdn-icons-png.flaticon.com",
-      "plus.unsplash.com",
-      "images.unsplash.com",
-      "ai-tool.nextjstemplates.com",
-      "vitaway.org",
-      "upload.wikimedia.org",
-      "play.google.com",
-      "lh3.googleusercontent.com",
-      "diplo-media.s3.eu-central-1.amazonaws.com",
-      "encrypted-tbn0.gstatic.com",
-      "tefconnect.com",
-      "127.0.0.1",
-      "ehr.vitaway.org",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ai-tool.nextjstemplates.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'vitaway.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'diplo-media.s3.eu-central-1.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tefconnect.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ehr.vitaway.org',
+        pathname: '/**',
+      },
     ],
   },
 };
