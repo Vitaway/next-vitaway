cb3f907e80fca60e62ad988a587b82df1738612f
patiencemanzen <hseal419@gmail.com>
2025-06-11 16:30:01 +0200

    fix: Update App Store link in Download page for accurate redirection

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -60,7 +60,7 @@ function Download() {
                   </p>
                   <hr className="my-8 border-gray-600" />
                   <div className="flex items-center mb-3 sm:justify-left">
-                    <a href="https://play.google.com/store/apps/details?id=org.vitaway.app" target='__blank' className="mr-3 transition duration-300 hover:shadow-lg">
+                    <a href="https://apps.apple.com/rw/app/vitaway-plus/id6745190821" target='__blank' className="mr-3 transition duration-300 hover:shadow-lg">
                       <Image width={100} height={100} src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1000px-Download_on_the_App_Store_Badge.svg.png" className="object-cover object-top w-32 mx-auto" alt="Download Vitaway From App Store" />
                     </a>
                     <a href="https://play.google.com/store/apps/details?id=org.vitaway.app" target='__blank' className="transition duration-300 hover:shadow-lg">
