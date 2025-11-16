82c035d04af49c0c6b1b16e21796d057de0cc1e8
patiencemanzen <hseal419@gmail.com>
2025-11-16 15:15:25 +0200

    refactor: remove Prisma schema and related dependencies; update sitemap with new URLs and last modified dates

--- app/components/admin/admin-navbar.tsx
+++ app/components/admin/admin-navbar.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import Image from 'next/image'
 import React from 'react';
-import { useSession, signOut } from 'next-auth/react';
 import Logo from '../logo';
 
 
 function AdminNavbar() {
-    const { data: session } = useSession();
-
     return (
         <nav className="w-full h-16 bg-white shadow-sm flex items-center px-4 border-b border-gray-200">
             <div className="flex justify-between items-center w-full">
@@ -21,13 +17,7 @@ function AdminNavbar() {
                 </div>
                 <div>
                     <div className="flex items-center space-x-2">
-                        {session && (
-                            <>
-                                <Image src={session.user?.image || ""} className="w-7 h-7 rounded-full object-cover" alt="Logo" width={40} height={40} />
-                                <span className="text-sm text-slate-600 font-semibold">Hi, {session.user?.name}</span>
-                                <button onClick={() => signOut()} className="bg-red-600 text-white px-4 py-2 rounded text-sm">Logout</button>
-                            </>
-                        )}
+                        <span className="text-sm text-slate-600 font-semibold">Admin Panel</span>
                     </div>
                 </div>
             </div>
